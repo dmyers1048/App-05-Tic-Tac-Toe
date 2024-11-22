76bd2a251ec1689fe1f55e1e702f14c8b82f3ae0
dmyers1048 <dmyers1@bsd220.org>
2024-11-22 09:36:49 -0600

    Initial Commit

---
+++ App 05 Tic Tac Toe/App_05_Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_05_Tic_Tac_ToeApp.swift
+//  App 05 Tic Tac Toe
+//
+//  Created by Devan Myers on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct App_05_Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App 05 Tic Tac Toe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App 05 Tic Tac Toe
+//
+//  Created by Devan Myers on 11/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
